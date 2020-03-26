41884b95eefc292f9cfdfef0c9a30ed4e44a740b
Deepak Kumar <kdeepak2912@gmail.com>
2020-03-26 15:26:41 +0530

    exams scraper

--- scraper_exams.py
+++ scraper_exams.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 import requests
 from bs4 import BeautifulSoup
@@ -24,18 +25,35 @@ def ins_query_maker(tablename, rowdict):
 
 def write_json(institute, university_name=""):
     json_object = json.dumps(institute, indent=2)
-    filename = "json/" + university_name + "_data.json"
+    filename = "out_files/json/exams/" + university_name + "_data.json"
     with open(filename, "w") as outfile:
         outfile.write(json_object)
 
-def scrape_courses( url):
+def scrape_exams( url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
     insert_queries = []
 
-    exams = {}
+    exams_entrance = {}
     try:
-        pass
+        exams__ = soup.find_all('table', attrs={"class": "Styled__TableStyle-sc-10ucg51-0 lfnXDf"})[1]
+        # print(exams__.text)
+        exams_ = exams__.find_all('tr')
+        for _ in exams_:
+            if _.find(text=re.compile("Exams")):
+                exams = _
+                break
+        exam = exams.find_all('div', attrs={"class": "Styled__EntryReqDetails-sc-14wej5r-4 jcAfxi"})
+        for eexam in exam:
+            print(eexam.label.text)
+            print(eexam.span.text)
+            exams_entrance.update({
+                eexam.label.text: eexam.span.text,
+            })
+        # for link in links:
+        #     if link.find(text=re.compile("Edit")):
+        #         thelink = link
+        #         break
     except IndexError:
         pass
     except TypeError:
@@ -45,15 +63,20 @@ def scrape_courses( url):
     try:
         exam = {
             "institutes_instituteexamrelation": {
-                "exam": None
+                "toefl": exams_entrance.get("TOEFL :"),
+                "ielts": exams_entrance.get("IELTS :"),
+                "pte": exams_entrance.get("PTE :"),
+                "gre": exams_entrance.get("GRE :"),
+                "gmat": exams_entrance.get("GMAT :"),
             }
         }
         insert_queries.append(ins_query_maker("institutes_instituteexamrelation", exam["institutes_instituteexamrelation"]))
-        return  insert_queries
+        return insert_queries
     except IndexError:
         pass
     finally:
-        write_json(exams, url.split('/')[-1])
+        pass
+        # write_json(exams, url.split('/')[-1])
 
 if __name__ == "__main__":
     urls = [
@@ -66,15 +89,14 @@ if __name__ == "__main__":
 
     database = DBQueries()
     conx = database.connect("Courses")
-    create_queries = [f"CREATE TABLE insinstitutes_institutecourse(department VARCHAR(100), degree_id VARCHAR(100), stream_id VARCHAR(100), name VARCHAR(100), level_id VARCHAR(100))"]
-    for query in create_queries:
-        database._create_table(conx, query)
+    # create_queries = f"CREATE TABLE institutes_instituteexamrelation(toefl VARCHAR(100), ielts VARCHAR(100), pte VARCHAR(100), gre VARCHAR(100), gmat VARCHAR(100))"
+    # database._create_table(conx, create_queries)
     try:
         for url in urls:
             print('scraping ', url)
-            insert_queries = scrape_courses(url)
-            # for query in insert_queries:
-            #     database.insert_record(conx, query)
+            insert_queries = scrape_exams(url)
+            for query in insert_queries:
+                database.insert_record(conx, query)
     except ConnectionError:
         pass
     finally:
